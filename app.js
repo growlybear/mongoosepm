0c94d814cbe60afe1349573f55d577b9d6d12188
Michael Allan <michaelallan@optushome.com.au>
2013-12-24 16:23:26 +1100

    Add database connection to the app

--- app.js
+++ app.js
@@ -4,8 +4,11 @@
  */
 
 var express = require('express');
-var routes = require('./routes');
+var db      = require('./models/db');
+var routes  = require('./routes');
+
 var user = require('./routes/user');
+
 var http = require('http');
 var path = require('path');
 
