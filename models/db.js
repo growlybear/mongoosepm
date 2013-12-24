110e191fd29240a222fa4e1c23309086e4a56d0e
Michael Allan <michaelallan@optushome.com.au>
2013-12-24 16:19:53 +1100

    Specify mongoose connection and behaviours

---
+++ models/db.js
@@ -0,0 +1,28 @@
+var mongoose = require('mongoose');
+
+// build the connection string
+var dbURI = 'mongodb://localhost/MongoosePM';
+
+// create the database connection
+mongoose.connect(dbURI);
+
+// handle various database events
+mongoose.connection.on('connected', function () {
+    console.log('Mongoose connected to: ', dbURI);
+});
+
+mongoose.connection.on('error', function (err) {
+    console.log('Mongoose connection error: ', err);
+});
+
+mongoose.connection.on('disconnected', function () {
+    console.log('Mongoose disconnected');
+});
+
+// handle unexpected process termination
+process.on('SIGINT', function (0 {
+    mongoose.connection.close(function () {
+        console.log('Mongoose disconnected due to application termination');
+        process.exit(0);
+    });
+}));
