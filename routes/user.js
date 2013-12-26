c6bb57fda6ef3d2aee97a704f361a2bbdf1fcece
Michael Allan <michaelallan@optushome.com.au>
2013-12-26 20:30:42 +1100

    Implement GET request for /user/new

--- routes/user.js
+++ routes/user.js
@@ -1,2 +1,9 @@
 var mongoose = require( 'mongoose' );
 var User = mongoose.model( 'User' );
+
+exports.create = function (req, res) {
+    res.render('user-form', {
+        title: 'Create user',
+        buttonText: 'Join!'
+    });
+};
