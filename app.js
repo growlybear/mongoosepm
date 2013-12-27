e173b96be1070ea7477459ec9f61439e9e3b7df9
Michael Allan <michaelallan@optushome.com.au>
2013-12-28 09:47:28 +1100

    Add 404 response and error handler

--- app.js
+++ app.js
@@ -72,6 +72,47 @@ app.get( '/project/delete/:id', project.confirmDelete );
 app.post('/project/delete/:id', project.doDelete );
 
 
+// assume 404 at this point as nothing else responded
+app.use(function(req, res, next){
+    res.status(404);
+
+    // respond with html page
+    if (req.accepts('html')) {
+    res.render('404', { url: req.url });
+    return;
+    }
+
+    // respond with json
+    if (req.accepts('json')) {
+    res.send({ error: 'Not found' });
+    return;
+    }
+
+    // default to plain-text. send()
+    res.type('txt').send('Not found');
+});
+
+// error-handling middleware require an
+// arity of 4, ie. the signature (err, req, res, next).
+// when connect has an error, it will invoke ONLY error-handling
+// middleware.
+
+// If we were to next() here any remaining non-error-handling
+// middleware would then be executed, or if we next(err) to
+// continue passing the error, only error-handling middleware
+// would remain being executed, however here
+// we simply respond with an error page.
+
+app.use(function(err, req, res, next){
+    // we may use properties of the error object
+    // here and next(err) appropriately, or if
+    // we possibly recovered from the error, simply next().
+    res.status(err.status || 500);
+    // TODO implement this
+    res.render('500', { error: err });
+});
+
+
 // go!
 http.createServer(app).listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
